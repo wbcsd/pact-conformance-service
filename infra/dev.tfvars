24e749dc144f0d9898178750c75d8327142eefee
Gertjan <gertjan@snok.nl>
2025-08-26 00:17:50 +0200

    chore: remove obsolete deployment scripts and Terraform configurations

--- infra/dev.tfvars
+++
@@ -1,3 +0,0 @@
-environment = "dev"
-region      = "eu-north-1"
-webhook_url = "https://conformance.services.dev.carbon-transparency.org"
--- infra/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "api_endpoint" {
-  description = "The URL of the deployed HTTP API"
-  value       = aws_apigatewayv2_api.http_api.api_endpoint
-}
